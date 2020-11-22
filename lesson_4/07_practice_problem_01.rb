564b845f1c839979c963ccb6a6ed78ca63b97f33
S.Rola <roladasilva@gmail.com>
2020-11-22 12:14:00 +0000

    Small update to lessons 2 & 4

--- lesson_4/07_practice_problem_01.rb
+++ lesson_4/07_practice_problem_01.rb
@@ -2,7 +2,3 @@ flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 flintstones_hash = {}
 
 flintstones.each_with_index { |name, index| flintstones_hash[name] = index }
-
-
-p flintstones_hash
-
--- lesson_4/07_practice_problem_08.rb
+++ lesson_4/07_practice_problem_08.rb
@@ -5,7 +5,8 @@ numbers.each do |number|
 end
 
 # output: 1, 3
-# We should never modify and iterate an array at the same time. As we use the shift method the array elements are reduced, meaning that we will skip some iterations.
+# We should never modify and iterate an array at the same time. As we use the shift method the array elements are reduced, meaning that we will skip some iterations(indexes). The removal of the first item in the first pass changes the value found for the second pass and so on.
+
 
 numbers = [1, 2, 3, 4]
 numbers.each do |number|
@@ -13,4 +14,4 @@ numbers.each do |number|
   numbers.pop(1)
 end
 
-# output: 1 2
+# output: 1, 2
